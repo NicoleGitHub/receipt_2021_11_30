4738eca27dcbb4833685846fa2c80f64d636b720
pangni <94227117+NicoleGitHub@users.noreply.github.com>
2021-11-30 18:39:32 +0800

    init commit

---
+++ reciept/src/main/java/pos/machine/PosMachine.java
@@ -0,0 +1,83 @@
+package pos.machine;
+
+
+import com.sun.tools.javac.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PosMachine {
+
+    public String printReceipt(List<String> barcodes) {
+//        return generateReceiptList(getBarcodeQuantity(barcodes));
+        return null;
+    }
+
+//    public List<Pair<String, Integer>> getBarcodeQuantity(List<String> barcodes) {
+//        List<String> distinctBarcodes = barcodes.stream().distinct().collect(Collectors.toList());
+//        List<Pair<String, Integer>> distinctBarcodeWithQuantity = new ArrayList<>();
+//
+//        for(String distinctBarcode: distinctBarcodes) {
+//            Integer countBarcode = 0;
+//            for(String barcode: barcodes) {
+//                if(distinctBarcode.equals(barcode)) {
+//                    countBarcode++;
+//                }
+//            }
+//            Pair<String, Integer> pair = new Pair<>(distinctBarcode, countBarcode);
+//            distinctBarcodeWithQuantity.add(pair);
+//        }
+//
+//        return distinctBarcodeWithQuantity;
+//    }
+//
+//    public String generateReceiptList(List<Pair<String, Integer>> barcodes) {
+//
+//        String receiptList = "***<store earning no money>Receipt***\n";
+//
+//        for(Pair<String, Integer> barcode: barcodes) {
+//            receiptList += generateReceiptLine(barcode);
+//        }
+//
+//        receiptList += "----------------------\n" +
+//                "Total: 24 (yuan)\n" +
+//                "**********************";
+//
+//        return receiptList;
+//    }
+//
+//    public String generateReceiptLine(Pair<String, Integer> barcodeQuantityPair) {
+//
+//        ItemInfo itemInfo = findItemBarCodeFromDB(barcodeQuantityPair.fst);
+//
+//        return "Name: "+
+//                itemInfo.getName()+
+//                ", Quantity: "+
+//                barcodeQuantityPair.snd+
+//                ", Unit price: "+
+//                itemInfo.getPrice() +
+//                " (yuan), Subtotal: "+ calculateSubtotal(itemInfo, barcodeQuantityPair.snd) +
+//                " (yuan)\n";
+//    }
+//
+//    public ItemInfo findItemBarCodeFromDB(String barcode) {
+//        List<ItemInfo> database = ItemDataLoader.loadAllItemInfos();
+//
+//        for(ItemInfo itemInfo : database) {
+//            if(itemInfo.getBarcode().equals(barcode)) {
+//                return itemInfo;
+//            }
+//        }
+//
+//        return null;
+//    }
+//
+//    public int calculateSubtotal(ItemInfo item, int unit) {
+//        return item.getPrice()*unit;
+//    }
+
+
+
+
+}
